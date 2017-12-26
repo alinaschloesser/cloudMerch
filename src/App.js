5affa1340095fde568a836011f2bcfdb936f37ec
Alina <alina.m.schloesser@gmail.com>
2017-12-26 14:07:57 -0500

    header fixed -app.js back to previous commit

--- src/App.js
+++ src/App.js
@@ -13,21 +13,26 @@ import LocalLogin from './components/routes/LocalLogin';
 import AddNewStore from './components/routes/AddNewStore';
 import './css/style.css';
 
-const App = () =>{
-	return (
-		<Router>
-			<div>
-				<Route exact path="/" component={Home} />
-				<Route path="/user-login/:id" component={UserView} />
-				<Route path="/shop/:id" component={Shop} />
-				<Route path="/checkout" component={Checkout} />
-				<Route path="/signup" component={Signup} />
-				<Route path="/search-results/:tag" component={SearchResults} />
-				<Route path="/local-login" component={LocalLogin} />
-				<Route path="/add-store/:id" component={AddNewStore} />
-			</div>
-		</Router>
-	);
+class App extends Component {
+	componentDidMount() {		
+		this.props.fetchUser();		
+	}
+	render(){
+		return (
+			<Router>
+				<div>
+					<Route exact path="/" component={Home} />
+					<Route path="/user-login/:id" component={UserView} />
+					<Route path="/shop/:id" component={Shop} />
+					<Route path="/checkout" component={Checkout} />
+					<Route path="/signup" component={Signup} />
+					<Route path="/search-results/:tag" component={SearchResults} />
+					<Route path="/local-login" component={LocalLogin} />
+					<Route path="/add-store/:id" component={AddNewStore} />
+				</div>
+			</Router>
+		);
+	}
 };
 
-export default hot(module)(App);
+export default connect(null, actions)(App);
